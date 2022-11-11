7474a302e1a0e88685ca207c1884b0159c7f61d6
20BD1A05AE <20bd1a05aecsef@gmail.com>
2022-11-11 10:33:29 +0530

    This is my first commit to JenkinsMavenPipeline Project

---
+++ src/main/java/com/kmit/SampleProject/App.java
@@ -0,0 +1,13 @@
+package com.kmit.SampleProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
